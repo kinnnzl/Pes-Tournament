33c0ee4ebfd6f5383fc3974960f7b0c576e85058
Kittithee Thaweesuk <zkittithee.t@pttdigital.com>
2022-11-11 00:01:10 +0700

    Add allow CORS

--- pes-tournament-server/server.js
+++ pes-tournament-server/server.js
@@ -15,6 +15,13 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
+app.use(function (req, res, next) {
+
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const db = require("./app/models");
 db.sequelize.sync({ force: false }).then(() => {
     console.log("Api start");
